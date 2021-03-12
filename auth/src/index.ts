612296f14ba996352085045469fb0ad1a8114c90
Qingyuan Ma <qma@ath050325.corp.athenahealth.com>
2021-03-12 14:07:56 -0500

    init auth app

---
+++ auth/src/index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import { json } from 'body-parser';
+
+
+const app = express();
+app.use(json());
+
+app.listen(
+    3000, () => {
+        console.log('auth listening on port 3000')
+    }
+);
+
