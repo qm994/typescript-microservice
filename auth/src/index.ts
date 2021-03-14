b12489795845bcd610515e854d37c3c195ec0c8a
qm994 <qm28@georgetown.edu>
2021-03-13 20:45:18 -0500

    add ingree config

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,9 +5,11 @@ import { json } from 'body-parser';
 const app = express();
 app.use(json());
 
-app.listen(
-    3000, () => {
-        console.log('auth listening on port 3000')
-    }
-);
+app.get('/api/users/currentuser', (req, res) => {
+    res.send("Hi there!")
+})
+
+app.listen(3000, () => {
+    console.log('auth listening on port 3000')
+});
 
