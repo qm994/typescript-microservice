efbcb4fa3e3e1c487152e76eab41bc61359ab79a
Qingyuan Ma <qma@athenahealth.com>
2021-07-03 16:21:21 -0400

    setup mongodb service and use mongoose in node.js

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
-
+import mongoose from "mongoose";
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
@@ -23,6 +23,20 @@ app.all("*", () => {
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log("auth listening on port 3000!!!!!!!!!");
-});
+const start = async () => {
+  try {
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+
+  app.listen(3000, () => {
+    console.log("auth listening on port 3000!!!!!!!!!");
+  });
+};
+
+start();
