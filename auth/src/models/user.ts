0c1da4d2b6eca12aa11de56bfc986a1f7a3f88ee
Qingyuan Ma <qma@athenahealth.com>
2021-07-04 20:31:10 -0400

    integrate ts type checking for User model mongoose + 1

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -8,6 +8,13 @@ interface UserAttrs {
   password: string;
 }
 
+// An interface that describes the properties
+// that a User model has
+
+interface UserModel extends mongoose.Model<any> {
+  build(attrs: UserAttrs): any;
+}
+
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
@@ -19,10 +26,11 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-const User = mongoose.model("User", userSchema);
-
-const buildUser = (attrs: UserAttrs) => {
+// Add static built in function to the model
+userSchema.statics.build = (attrs: UserAttrs) => {
   return new User(attrs);
 };
+
+const User = mongoose.model<any, UserModel>("User", userSchema);
 
 export { User };
